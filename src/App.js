119d560c082c8d7aeba6801429309b5c6926e28c
Santiago Sacristan <sacristan.santiago@gmail.com>
2021-09-08 22:24:56 -0300

    primeros cambios

--- src/App.js
+++ src/App.js
@@ -5,18 +5,34 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Bienvendo a mi sitio web!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Mi nombre es Santiago y a continuacion encontrarás un link a mi repositorio de GitHub:
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/sacristan-santiago"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Mi Repositorio
         </a>
+        <p>
+          Y también links a mi proyectos previos!!!
+        </p>
+        <a
+          className="App-link"
+          href="https://sacristan-santiago.github.io/javascript_interactuando_DOM/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Mi caja registradora</a>
+        <a
+          className="App-link"
+          href="https://sacristan-santiago.github.io/Consultorio-Neurologia/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >Consultorio de Neurologia</a>
       </header>
     </div>
   );
