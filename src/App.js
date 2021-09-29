b675223943722563f09f181e84c8ef98e1402497
Santiago Sacristan <sacristan.santiago@gmail.com>
2021-09-29 18:41:57 -0300

    se confiura browserRouter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import './App.css';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Nav from './components/NavBar';
 import PaisListContainer from './components/PaisListContainer';
 import ItemDetailCointainer from './components/ItemDetailContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <BrowserRouter >
+      <div className="App">
         <Nav />
-      </header>
-      <main>
-        <h1>Bienvendo a Swap!</h1>
-        <PaisListContainer paises={["Argentina", "Chile", "Peru", "Bolivia", "Uruguay"]}/>
-        <p>   
-          En swap podrás intercambiar tu casa con la de otra persona en cualquier parte del mundo!
-        </p>
-        <p>   
-          Y como somos muy proactivos tambien vendemos algunos productos que podes llevarte de recuerdo de tus viajes!
-        </p>
-        <ItemDetailCointainer />
-      </main>
-    </div>
+        <Switch>
+          <Route exact path="/">
+            <h1>Bienvendo a Swap!</h1>
+            <PaisListContainer paises={["Argentina", "Chile", "Peru", "Bolivia", "Uruguay"]}/>
+            <p>   
+            En swap podrás intercambiar tu casa con la de otra persona en cualquier parte del mundo!
+            </p>       
+          </Route>
+          <Route path="/categoria/:id">
+            <h1>Bienvendo a Swap!</h1>
+            <PaisListContainer paises={["Argentina", "Chile", "Peru", "Bolivia", "Uruguay"]}/>
+            <p>   
+            En swap podrás intercambiar tu casa con la de otra persona en cualquier parte del mundo!
+            </p> 
+          </Route>
+          <Route path="/item/:id">
+            <ItemDetailCointainer />
+          </Route>
+        </Switch>        
+          
+
+      </div>
+    </BrowserRouter>
+    
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,14 +1,13 @@
-import CartWidget from './CartWidjet'
+import CartWidget from './CartWidjet';
+import { NavLink } from 'react-router-dom';
 
 const Nav = () => {
     return (
         <nav className="header_nav">
             <h1>Swap</h1>
             <ul>
-                <li>Inicio</li>
-                <li>Swaps</li>
-                <li>Nosotros</li>
-                <li>Contacto</li>
+                <li><NavLink activeclassName="currentCategory" className="nav-link" to="/categoria/1">Categoria 1</NavLink></li>
+                <li><NavLink activeclassName="currentCategory" className="nav-link" to="/categoria/2">Categoria 2</NavLink></li>
             </ul>
             <CartWidget />
         </nav>
