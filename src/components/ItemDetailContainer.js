133b4ef40fb88600e8eae67754f72bb15ca56e64
Santiago Sacristan <sacristan.santiago@gmail.com>
2021-10-13 23:48:41 -0300

    se incopora firebase

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -1,53 +1,31 @@
+import { app } from '../firebase';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import ItemDetail from './ItemDetail.js';
+import { getDocs, collection } from '@firebase/firestore';
+import { getFirestore } from 'firebase/firestore';
 
 const ItemDetailCointainer = () => {
     const [items, setItems] = useState ([])
     const {name} = useParams();
-    
-    const mockItems  = [
-        {
-            id: 1, 
-            title: "Pimenton",
-            description: "Crackers a base de harina de arroz sabor pimenton",
-            price: 200.00,
-            stock: 10,
-            image: "/pimenton.jpg"
-        },
-        {
-            id: 2, 
-            title: "Mediterraneas",
-            description: "Crackers a base de harina de arroz sabor mediterráneo",
-            price: 200.00,
-            stock: 10,
-            image: "/mediterraneas.jpg"
-        },
-        {
-            id: 3, 
-            title: "Carbon",
-            description: "Crackers a base de harina de arroz sabor carbon ahumado",
-            price: 200.00,
-            stock: 10,
-            image: "/carbon.jpg"
-        },
-        {
-            id: 3, 
-            title: "Semillas",
-            description: "Crackers a base de harina de arroz con mix de semillas",
-            price: 200.00,
-            stock: 10,
-            image: "/semillas.jpg"
-        }]
 
-    useEffect(()=>{
-        //este fetch deberia traer de la API el producto con :id=id
-        // fetch('https://fakestoreapi.com/products/')
-        //     .then(res=>res.json())
-        //     .then(data=>setItems(data))
-        setItems(mockItems);
-        console.log(name)
-        console.log(items.find(item=>item.title === name))
+    useEffect(async () => {
+        try {
+            const db = getFirestore(app);
+            const itemsCollection = collection(db, "items")
+            const itemsSnapshot = await getDocs(itemsCollection);
+            const itemsList = itemsSnapshot.docs.map(doc => doc.data());
+    
+            if (!collection.size === 0) {
+                console.log("Collection does not exist");
+                return;
+            }
+            console.log("Collection found");
+            setItems(itemsList)
+            console.log(items)
+        } catch (err) {
+            console.log("Error serching items", err)
+        }
     }, [])
 
     if (items.length<=0) {
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+export const app = initializeApp({
+    apiKey: "AIzaSyCsqDOpjZQ9IIEvJGfBYGRTwjwnNHm7hzI",
+    authDomain: "shiva-53202.firebaseapp.com",
+    projectId: "shiva-53202",
+    storageBucket: "shiva-53202.appspot.com",
+    messagingSenderId: "1021999300277",
+    appId: "1:1021999300277:web:1d03b30e5d2e63840888fc"
+  });
+
